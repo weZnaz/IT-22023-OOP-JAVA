09f7bc1d711d8cb4ba3b12417d32e9f32c452966
Syed Nazmul Islam <syednazmulislamramim@gmail.com>
2024-11-05 08:00:24 +0600

    Create summ.java

---
+++ summ.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class summ{
+      public static void main(String[] args){
+
+try{
+
+   File file=new File("file.txt");
+Scanner sc=new Scanner(file);
+if(sc.hasNextLine()){
+String line=sc.nextLine();
+String[] arr=line.split(",");
+for(int i=0;i<arr.length;i++){
+int n=Integer.parseInt(arr[i].trim());
+StringBuilder seris=new StringBuilder();
+for(int j=1;j<=n;j++){
+seris.append(j);
+if(j<n){
+seris.append("+");
+}
+}
+int sum=n*(n+1)/2;
+System.out.println(seris+"="+sum);
+
+
+}
+}sc.close();
+}catch(FileNotFoundException e){System.out.println("File not found.");}
+catch(NumberFormatException e){System.out.println("Invalid number format.");}
+
+
+
+}
+
+}
